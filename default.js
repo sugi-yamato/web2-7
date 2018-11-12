c3a1f569038ec6937fef5ef4eac8d10320a18f69
sugi-yamato <38421455+sugi-yamato@users.noreply.github.com>
2018-11-12 17:40:27 +0900

    Create default.js

---
+++ default.js
@@ -0,0 +1,13 @@
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム。０ｰ２の数字を入力してください！'));
+
+var message;
+if(answer === number){
+    message = 'あたり';
+}else if(answer < number){
+    message = '残念！もっと大きいです！';
+}else if(number < answer){
+    message = '残念もっと小さいです！';
+}else{
+    message = '0～3の数字を入力してください。';
+}
